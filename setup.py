1db8c9706d4e8216204fede30be9bfcb30f54bcf
dorinapetra <dorinapetra@gmail.com>
2023-11-20 09:31:03 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='hunsum-eval',
     version='1.0',
-    # packages=['hunsum_eval', 'hunsum_eval.utils', 'hunsum_eval.embeddings', 'hunsum_eval.entrypoints',
-    #          'hunsum_eval.metrics'],
+    packages=find_packages(
+        # All keyword arguments below are optional:
+        where='hunsum_eval',  # '.' by default
+        # include=['mypackage*'],  # ['*'] by default
+        # exclude=['mypackage.tests'],  # empty by default
+    ),
+    package_dir={'': 'hunsum_eval/tests'},
     url='',
     license='',
     author='Dorina Lakatos',
