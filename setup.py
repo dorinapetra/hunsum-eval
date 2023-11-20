c3459440d2c38dc0e21ba562f9f9acf716247a08
dorinapetra <dorinapetra@gmail.com>
2023-11-20 09:29:04 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='hunsum-eval',
     version='1.0',
-    packages=['hunsum_eval', 'hunsum_eval.utils', 'hunsum_eval.embeddings', 'hunsum_eval.entrypoints',
-              'hunsum_eval.metrics'],
+    # packages=['hunsum_eval', 'hunsum_eval.utils', 'hunsum_eval.embeddings', 'hunsum_eval.entrypoints',
+    #          'hunsum_eval.metrics'],
     url='',
     license='',
     author='Dorina Lakatos',
